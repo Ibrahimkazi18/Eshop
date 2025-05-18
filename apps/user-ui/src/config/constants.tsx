ac256c45d04ef3cf150c9a2db34d50cc9b51dd1e
Ibrahim <ibirfkazi@gmail.com>
2025-05-18 16:43:52 +0530

    add navbar, signin-up frontend

---
+++ apps/user-ui/src/config/constants.tsx
@@ -0,0 +1,22 @@
+export const navItems: NavItemType[] = [
+    {
+        title: "Home",
+        href: "/"
+    },
+    {
+        title: "Products",
+        href: "/products"
+    },
+    {
+        title: "Shops",
+        href: "/shops"
+    },
+    {
+        title: "Offers",
+        href: "/offers"
+    },
+    {
+        title: "Become A Seller",
+        href: "/become-a-seller"
+    },
+]
